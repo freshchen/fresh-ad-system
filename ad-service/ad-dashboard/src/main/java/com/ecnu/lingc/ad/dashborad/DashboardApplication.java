cef10185fd8752e7f053ec00f0f05ce75b34357c
freshchen <961011595@qq.com>
2019-07-10 21:17:19 +0800

    [fresh] ad dashborad

---
+++ ad-service/ad-dashboard/src/main/java/com/ecnu/lingc/ad/dashborad/DashboardApplication.java
@@ -0,0 +1,23 @@
+package com.ecnu.lingc.ad.dashborad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @program: fresh-ad-system
+ * @Date: 2019/6/30 18:31
+ * @Author: Ling Chen
+ * @Description:
+ */
+@EnableEurekaClient
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DashboardApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+}
